df06fc86d9a8ae2cf3a18464bf4133395b5fa969
Akshatbari02 <akshatbari.ab@gmail.com>
2025-01-18 20:58:18 +0530

    promptquest

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {useEffect, useState} from 'react';
+import {useEffect, useState, Suspense} from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter,useSearchParams } from 'next/navigation';
 import Form from '@components/Form';
@@ -60,6 +60,7 @@ const EditPrompt = () => {
         }
     }
   return (
+    <Suspense fallback={<div>Loading...</div>}>
         <Form
             type= "Edit"
             post= {post}
@@ -67,7 +68,7 @@ const EditPrompt = () => {
             submitting= {submitting}
             handleSubmit= {updatePrompt}
         />
-    
+    </Suspense>
 )
 }
 
