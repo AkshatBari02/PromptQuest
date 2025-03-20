3f509c1d4d0b0490945ad52364f9b2c42aec6131
Akshatbari02 <akshatbari.ab@gmail.com>
2025-03-20 15:50:10 +0530

    updated

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,32 +7,39 @@ import { usePathname, useRouter } from "next/navigation";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState("");
-  const{data: session} = useSession();
+  const { data: session } = useSession();
   const pathName = usePathname();
   const router = useRouter();
 
   const handleCopy = () => {
-    setCopied(post.prompt);
-    navigator.clipboard.writeText(post.prompt);
+    if (session?.user.id) {
+      setCopied(post.prompt);
+      navigator.clipboard.writeText(post.prompt);
+    } else {
+      alert("Please login to copy the prompt !!");
+    }
     setTimeout(() => setCopied(""), 3000);
   };
 
-  const handleProfileClick = ()=>{
-    if(post.creator._id === session?.user.id){
-      return router.push('/profile')
+  const handleProfileClick = () => {
+    if (post.creator._id === session?.user.id) {
+      return router.push("/profile");
     }
-    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`)
-  }
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+  };
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
-        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer" onClick={() => {
-  if (session?.user?.id) {
-    handleProfileClick();
-  } else {
-    alert("To see other's profile, SIGN IN first!!");
-  }
-}}>
+        <div
+          className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
+          onClick={() => {
+            if (session?.user?.id) {
+              handleProfileClick();
+            } else {
+              alert("To see other's profile, SIGN IN first!!");
+            }
+          }}
+        >
           <Image
             src={post.creator.image}
             alt="user_image"
@@ -73,12 +80,18 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         #{post.tag}
       </p>
 
-      {session?.user.id === post.creator._id && pathName === '/profile' && (
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
         <div className="mt-4 flex-center gap-4 border-t border-gray-100 pt-3">
-          <p className="font-inter text-sm green_gradient cursor-pointer" onClick={handleEdit}>
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
             Edit
           </p>
-          <p className="font-inter text-sm orange_gradient cursor-pointer" onClick={handleDelete}>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
             Delete
           </p>
         </div>
